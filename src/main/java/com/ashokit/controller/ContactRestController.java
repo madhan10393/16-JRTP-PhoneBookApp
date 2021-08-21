2fa49f4949b1ab3b1729293d2f9229f034982a73
madhan10393 <443madhan@gmail.com>
2021-08-21 21:50:09 +0530

    changing properti file and controller class

--- src/main/java/com/ashokit/controller/ContactRestController.java
+++ src/main/java/com/ashokit/controller/ContactRestController.java
@@ -1,9 +1,9 @@
 package com.ashokit.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ContactRestController {
 
 	@GetMapping("/index")
